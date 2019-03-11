6afc165d766d1e3c5ca575a5a444789adb687b2e
trainingns <gmartinez@nearsoft.com>
2019-03-11 12:45:39 -0600

    First commit

---
+++ TestAppCircleCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppCircleCI
+//
+//  Created by Guillermo Martinez Espina on 3/11/19.
+//  Copyright © 2019 Guillermo Martinez Espina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
